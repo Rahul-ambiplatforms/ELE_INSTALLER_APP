023a9e72e7147d7f49f5bc34a385805c808f8691
tushar45git <tusharthakre.vmukti@gmail.com>
2025-09-24 10:40:25 +0530

    Update App.js

--- Election-Seperate-Frontend-main/src/App.js
+++ Election-Seperate-Frontend-main/src/App.js
@@ -1,12 +1,9 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
-import { Box, Button, Container, Heading, Stack, Text } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { Box, Container } from '@chakra-ui/react';
 
 // Import your other components here
-// For example:
-// import Home from './components/Home';
-// import About from './components/About';
 import Login from './components/Login'; // Import your Rekha component
 import Header from './components/Header';
 import Head from './components/Head';
@@ -32,42 +29,48 @@ import EleUserDetails from './components/EleUserDetails';
 import BiharUsers from './components/BiharUsers';
 
 function App() {
-
   return (
     <Router>
-      {/* <Container maxW="container.xl"> */}
-      <Container maxW="100vw" p='0'>
-        <Box>
-          <Header />
-
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} /> {/*  */}
-            <Route path="/installer" element={<AutoInstaller />} /> 
-            <Route path="/autoinstaller" element={<AutoInstaller />} /> 
-            <Route path="/eleuserdetails" element={<EleUserDetails />} /> 
-            <Route path="/didinfo" element={<DidInfo />} /> 
-            <Route path="/punjabinstaller" element={<AutoInstaller />} /> 
-            {/* <Route path="/ai" element={<Ai />} /> */}
-            {/* <Route path="/aid" element={<AiDashboard />} /> */}
-            {/* <Route path="/aifeed" element={<Aifeed />} />  */}
-            <Route path="/pb" element={<Tripura />} /> 
-            <Route path="/head" element={<Head />} /> 
-            <Route path="/" element={<Login />} /> 
-            {/* <Route path="/attendance" element={<Attendance />} />  */}
-            <Route path="/eci" element={<Eci />} /> 
-            <Route path="/installed" element={<Installed />} /> 
-            <Route path="/eleuser" element={<ElectionInstaller />} /> 
-            <Route path="/support" element={<Support />} /> 
-
-            <Route path="/state/:state" element={<StatePage />} />
-            <Route path="/bihar/:state" element={<BiharUsers />} />
-            <Route path="/state/:state/:district" element={<DistrictPage />} />
-            <Route path="/state/:state/:district/:assemblyName" element={<AssemblyPage />} />
-
-          </Routes>
-        </Box>
-      </Container>
+      <AppContent />
     </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/';
+
+  return (
+    <Container maxW="100vw" p='0'>
+      <Box>
+        {!isLoginPage && <Header />}
+
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/installer" element={<AutoInstaller />} />
+          <Route path="/autoinstaller" element={<AutoInstaller />} />
+          <Route path="/eleuserdetails" element={<EleUserDetails />} />
+          <Route path="/didinfo" element={<DidInfo />} />
+          <Route path="/punjabinstaller" element={<AutoInstaller />} />
+          {/* <Route path="/ai" element={<Ai />} /> */}
+          {/* <Route path="/aid" element={<AiDashboard />} /> */}
+          {/* <Route path="/aifeed" element={<Aifeed />} />  */}
+          <Route path="/pb" element={<Tripura />} />
+          <Route path="/head" element={<Head />} />
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/attendance" element={<Attendance />} />  */}
+          <Route path="/eci" element={<Eci />} />
+          <Route path="/installed" element={<Installed />} />
+          <Route path="/eleuser" element={<ElectionInstaller />} />
+          <Route path="/support" element={<Support />} />
+
+          <Route path="/state/:state" element={<StatePage />} />
+          <Route path="/bihar/:state" element={<BiharUsers />} />
+          <Route path="/state/:state/:district" element={<DistrictPage />} />
+          <Route path="/state/:state/:district/:assemblyName" element={<AssemblyPage />} />
+        </Routes>
+      </Box>
+    </Container>
   );
 }
 
